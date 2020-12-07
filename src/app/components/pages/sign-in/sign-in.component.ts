ee9bcb62e5196a8b448a92d22f18885dfbd878c0
Yerbolat <twinspazyl@gmail.com>
2020-12-07 21:37:31 +0600

    day #2

--- src/app/components/pages/sign-in/sign-in.component.ts
+++ src/app/components/pages/sign-in/sign-in.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+// import {AuthService} from '../../../services/auth.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-in.component.scss']
 })
 export class SignInComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(
+    // private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.form = new FormGroup({
+      login: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required])
+    });
+  }
+
+  login() {
+    // this.authService
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class AuthService {
-  constructor() { }
+
+  api = '/api';
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAllUsers(data) {
+    this.http.get(this.api + `/users?email=${data.login}&phoneNumber=${data.login}`);
+  }
 }
